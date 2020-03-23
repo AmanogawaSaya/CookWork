9b40a1762630b13651bfce50e002a00d72d1b96b
Saya <601470429@qq.com>
2020-03-23 09:50:08 +0800

    finish GUI

---
+++ CoopWorkGUI/CoopWorkGUI/CoopWorkGUI.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma comment(lib,"CoopWork.lib")
+
+#include "head.h"
+#include <QtWidgets/QWidget>
+#include "ui_CoopWorkGUI.h"
+#include <QFile>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QAction>
+#include <QMenu>
+#include <QMessageBox>
+#include <QScrollArea>
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QDebug>
+#include <QLabel>
+
+
+class CoopWorkGUI : public QWidget
+{
+	Q_OBJECT
+
+public:
+	CoopWorkGUI(QWidget *parent = Q_NULLPTR);
+
+private:
+	Ui::CoopWorkGUIClass ui;
+	QString m_FilePath;
+	QString m_EditIndex;
+	MySet m_Points;
+	bool m_IsShowPoints;
+	bool isRepaint; 
+	void showList();
+	void editListItem(QListWidgetItem* item);
+	void onActionAdd();
+	void onActionDelete();
+	void painteGraphics();
+	void resizeWidget(int value);
+	bool eventFilter(QObject* obj, QEvent* ev);
+};
